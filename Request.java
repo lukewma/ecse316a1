bea75fa21ddb43fde1a41b4538b10efd338cd86b
l <luke.wenjun.ma@gmail.com>
2020-02-14 04:54:55 -0500

    rfx req

--- Request.java
+++ Request.java
@@ -3,37 +3,39 @@ import java.util.Random;
 
 public class Request {
 
-	private String domain;
+	private String dom;
 	private Qtype type;
+
 	public Request(String domain, Qtype type){
-		this.domain = domain;
+		this.dom = domain;
 		this.type = type;
 	}
 
-	public byte[] getRequest(){
-		int qNameLength = getQNameLength();
-		ByteBuffer req = ByteBuffer.allocate(12 + 5 + qNameLength);
-		req.put(createRequestHeader());
-		req.put(createQuestionHeader(qNameLength));
+	public byte[] getReq(){
+		int qLen = getqLen();
+		ByteBuffer req = ByteBuffer.allocate(12 + 5 + qLen);
+		req.put(createReqH());
+		req.put(createQH(qLen));
         return req.array();
 	}
 
-	private byte[] createRequestHeader(){
-		ByteBuffer header = ByteBuffer.allocate(12);
+	private byte[] createReqH(){
+		//as per lab docs, 12 byte offset
+		ByteBuffer h = ByteBuffer.allocate(12);
 		byte[] randomID = new byte[2]; 
 		new Random().nextBytes(randomID);
-		header.put(randomID);
-		header.put((byte)0x01);
-		header.put((byte)0x00);
-		header.put((byte)0x00);
-		header.put((byte)0x01);
+		h.put(randomID);
+		h.put((byte)0x01);
+		h.put((byte)0x00);
+		h.put((byte)0x00);
+		h.put((byte)0x01);
 		
 		//lines 3, 4, and 5 will be all 0s, which is what we want
-		return header.array();
+		return h.array();
 	}
 	
 
-	private int getQNameLength(){
+	private int getqLen(){
 		int byteLength = 0;
 		String[] items = domain.split("\\.");
 		for(int i=0; i < items.length; i ++){
@@ -44,31 +46,31 @@ public class Request {
 		return byteLength;
 	}
 
-	private byte[] createQuestionHeader(int qNameLength){
-		ByteBuffer question = ByteBuffer.allocate(qNameLength+5);
+	private byte[] createQH(int qLen){
+		ByteBuffer q = ByteBuffer.allocate(qLen+5);
 		
 		//first calculate how many bytes we need so we know the size of the array
 		String[] items = domain.split("\\.");
 		for(int i=0; i < items.length; i ++){
-			question.put((byte) items[i].length());
+			q.put((byte) items[i].length());
 			for (int j = 0; j < items[i].length(); j++){
-				question.put((byte) ((int) items[i].charAt(j)));
+				q.put((byte) ((int) items[i].charAt(j)));
 				
 			}
 		}
 
-		question.put((byte) 0x00);
+		q.put((byte) 0x00);
 
 		//Add Query Type
-		question.put(hexStringToByteArray("000" + hexValueFromQtype(type)));
-		question.put((byte) 0x00);
+		q.put(hxStr2BytArr("000" + hexByQt(type)));
+		q.put((byte) 0x00);
 		//Add Query Class - always  0x0001 for internet addresses
-		question.put((byte) 0x0001);
+		q.put((byte) 0x0001);
 
-		return question.array();
+		return q.array();
 	}
 	
-	private char hexValueFromQtype(Qtype type){
+	private char hexByQt(Qtype type){
 		if (type == Qtype.A) {
 			return '1';
 		} else if (type == Qtype.NS) {
@@ -78,7 +80,7 @@ public class Request {
 		}
 	}
 
-	private static byte[] hexStringToByteArray(String s) {
+	private static byte[] hxStr2BytArr(String s) {
 		int len = s.length();
 	    byte[] data = new byte[len / 2];
 	    for (int i = 0; i < len; i += 2) {
