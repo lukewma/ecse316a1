2386cfef4855726e4dc34385bdb58f21b23b3bf5
l <luke.wenjun.ma@gmail.com>
2020-02-14 03:21:22 -0500

    added options, cli for last 2 args

--- Client.java
+++ Client.java
@@ -10,16 +10,43 @@ public class Client {
 
 	//TODO: change all the int = to Integer.equals or just change the Integer to an int?
 
+	//TODO: do these defaults work with cli options?
 	public QType qt = QType.A;
 	public int MAX_PACKET = 512;
 	private Integer timeout = new Integer(3000);				//default
-	private byte[] server = new byte[4];
+	private byte[] ip = new byte[4];
 	private Integer retries = new Integer(5);				//default
 	private String address;
-	private String name;
+	private String dom;
 	private Integer port = new Integer(53);
 
-	public Main(String args[]){
+	public Main(String args[]) throws Exception{
+
+		//TODO:change cli parse into a single function, change data fields
+		Options o = this.mkOpt();
+		CommandLineParser pars = new DefaultParser();
+		HelpFormatter frmt = new HelpFormatter();
+		CommandLine cmd;
+
+        try {
+            cmd = pars.parse(o, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+			frmt.printHelp("utility-name", o);
+        }
+
+        try {
+			this.parseIPDom(args);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		this.makeReq();
+			
+	}
+
+	public Options mkOpt(){
+
         Options options = new Options();
 
         Option timeout = Option.builder("t")
@@ -27,7 +54,7 @@ public class Client {
 			.long-opt("timeout")
 			.hasArg()
 			.numberOfArgs(1)
-			.type(Integer)
+			.type(Number.class)
 			.desc("Timeout in seconds")
 			.build();
 		options.addOption( timeout );
@@ -37,7 +64,7 @@ public class Client {
 			.long-opt("retries")
 			.hasarg()
 			.numberofargs(1)
-			.type(Integer)
+			.type(Number.class)
 			.desc("number of tries before error")
 			.build();
 		options.addOption( retries );
@@ -47,7 +74,7 @@ public class Client {
 			.long-opt("port")
 			.hasarg()
 			.numberofargs(1)
-			.type(Integer)
+			.type(Number.class)
 			.desc("port number")
 			.build();
 		options.addOption( port );
@@ -55,36 +82,80 @@ public class Client {
 		OptionGroup type = new OptionGroup();
 		
 		Option mx = Option.builder("mx")
-			
+			.long-opt("mail")
+			.required(false)
+			.hasarg(false)
+			.desc("set a mail server")
+			.build();
 
-        try {
-            Client testClient = new Client(args);
-            testClient.makeRequest();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+		Option ns = Option.builder("ns")
+			.long-opt("name")
+			.required(false)
+			.hasarg(false)
+			.desc("set a name server")
+			.build();
+
+		Option a = Option.builder("a")
+			.long-opt("ip")
+			.required(false)
+			.hasarg(false)
+			.desc("send ip address query (default)")
+			.build();
+
+		type.addOption(ns);
+		type.addOption(mx);
+		type.addOption(a);
+
+		options.addOptionGroup(type);		//use hasOption bool for cases
+
+		//because its not flags we'll just parse the rest of the input normally. CLI makes this too complicated
+		//Option serv = Option.builder("@")
+		//	.required(true)
+		//	.hasarg()
+		//	.numberOfArgs(4)
+		//	.valueSeparator('.')
+		//	type(Number[])
+		//options.addOption("@")
+
+		//Option domain = Option.builder
+
+		return options;
 	}
 
-	public Client(String args[]){
-		try {
-            this.parseInputArguments(args);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("ERROR\tIncorrect input syntax: Please check arguments and try again");
-        }
-        if (server == null || name == null) {
-            throw new IllegalArgumentException("ERROR\tIncorrect input syntax: Server IP and domain name must be provided.");
+	private void parseIPDom(String args[]){
+		List<String> argL= Arrays.asList(args);
+
+		this.dom = argL[argL.length - 1];
+		String[] octets;
+		if(argL[argl.length -2].contains("@")){
+			this.address = argL[argl.length -2].substring(1);
+			octets = address.split("\\.");
+
+			if(octets.length != 4) {
+				throw new IPFormatException("ERROR\tIncorrect input syntax: IP must contain 4 numbers between 0 and 255 inclusively, separated by a period (.)");
+			}
+
+			for(int i = 0; i < 4; i++){
+				int oct = Integer.parseInt(octets[i]);
+				if (oct < 0 || oct > 255) { 
+					throw new IPFormatException("ERROR\tIncorrect input syntax: IP must contain 4 numbers between 0 and 255 inclusively, separated by a period (.)");
+				}
+				ip[i] = (byte) oct;
+			}
+		}
+		else throw new MissingArgException("ERROR\tThere must be an IP address preceded by an '@' as the second last argument and a domain name as the last one")
 	}
 
     public void makeRequest() {
-        System.out.println("DnsClient sending request for " + name);
+        System.out.println("DnsClient sending request for " + dom);
         System.out.println("Server: " + address);
-        System.out.println("Request type: " + queryType);
+        System.out.println("Request type: " + QType);
         pollRequest(1);
     }
 
     private void pollRequest(int retryNumber) {				
-        if (retryNumber > maxRetries) {
-            System.out.println("ERROR\tMaximum number of retries " + maxRetries+ " exceeded");
+        if (retryNumber > retries) {
+            System.out.println("ERROR\tMaximum number of retries " + retries+ " exceeded");
             return;
         }
 
@@ -92,8 +163,8 @@ public class Client {
             //Create Datagram socket and request object(s)
             DatagramSocket socket = new DatagramSocket();
             socket.setSoTimeout(timeout);
-            InetAddress inetaddress = InetAddress.getByAddress(server);
-            DnsRequest request = new DnsRequest(name, queryType);
+            InetAddress inetaddress = InetAddress.getByAddress(ip);
+            DnsRequest request = new DnsRequest(name, QType);
 
             byte[] requestBytes = request.getRequest();
             byte[] responseBytes = new byte[1024];
@@ -110,6 +181,7 @@ public class Client {
 
             System.out.println("Response received after " + (endTime - startTime)/1000. + " seconds " + "(" + (retryNumber - 1) + " retries)");
 
+            //TODO: refactor appropriately
             DnsResponse response = new DnsResponse(responsePacket.getData(), requestBytes.length, queryType);
             response.outputResponse();
 
